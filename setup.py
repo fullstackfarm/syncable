3bb014b254884fd6c02db20af4266900533dea63
AJ Floersch <ajfloersch@gmail.com>
2025-06-22 13:25:53 -0500

    Recast version on local builds to avoid InvalidVersion errors.

--- setup.py
+++ setup.py
@@ -7,6 +7,16 @@ version = (
     .stdout.decode("utf-8")
     .strip()
 )
+
+if "-" in version:
+    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
+    # pip has gotten strict with version numbers
+    # so change it to: "1.3.3+22.git.gdf81228"
+    # See: https://peps.python.org/pep-0440/#local-version-segments
+    v, i, s = version.split("-")
+    version = v + "+" + i + ".git." + s
+
+assert "-" not in version
 assert "." in version
 
 with open("VERSION", "w", encoding="utf-8") as f:
